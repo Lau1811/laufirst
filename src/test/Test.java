8fd41fce08bdca636243007da4cfdcfeeb686edd
Administrator <1197856406@qq.com>
2018-02-07 21:40:14 +0800

    第一次提交

---
+++ src/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test {
+    int i = 88;
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
